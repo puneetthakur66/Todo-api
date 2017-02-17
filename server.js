a4a028f771645d31bc107fc782ee7608d9581260
Puneet Thakur <puneetthakur@3189241.no-reply.drupal.org>
2017-02-17 15:45:51 +0530

    underscore todo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app  = express();
 var PORT = process.env.PORT || 3000;
@@ -18,13 +19,14 @@ app.get('/todos', function (req, res) {
 
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	// var matchedTodo;
 
-	todos.forEach( function (todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	// todos.forEach( function (todo) {
+	// 	if (todoId === todo.id) {
+	// 		matchedTodo = todo;
+	// 	}
+	// });
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -36,7 +38,12 @@ app.get('/todos/:id', function (req, res) {
 
 app.post('/todos', function (req, res) {
 	var body = req.body;
+	body = _.pick(body, 'description', 'completed');
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0) {
+		return res.status(400).send();
+	}
 	body.id = nextId;
+	body.description = body.description.trim();
 	todos.push(body);
 	nextId++;
 	console.log('body data: ' + body);
