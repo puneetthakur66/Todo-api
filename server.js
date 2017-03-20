32c561b28a2b609f1cd3d14fb07fbab31cfe8a8a
Puneet Thakur <puneetthakur@3189241.no-reply.drupal.org>
2017-03-20 15:47:10 +0530

    User login sequlize customClass

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -124,6 +125,17 @@ app.post('/users', function (req, res) {
 	}, function (e) {
 		res.status(400).json(e);
 	});
+});
+
+app.post('/users/login', function (req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function (user){
+		res.json(user.toPublicJSON());
+	}, function(e){
+		res.status(401).send(e);
+	});
+
 });
 
 db.sequelize.sync({force: true}).then(function() {
